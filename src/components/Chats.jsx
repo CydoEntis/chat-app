c0078e5561cc820d87ac0903d4a5a3d04ba46094
Cody Stine <cydoentis@gmail.com>
2021-10-11 19:57:38 -0400

    Fixed minor bugs and hide project id and keys

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -7,78 +7,82 @@ import { useAuth } from '../contexts/AuthContext';
 import axios from 'axios';
 
 const Chats = () => {
-	const history = useHistory();
-	const { user } = useAuth();
+	const didMountRef = useRef(false);
 	const [loading, setLoading] = useState(true);
+	const { user } = useAuth();
+	const history = useHistory();
 
-	const getFile = async (url) => {
-		const response = await fetch(url);
-		const data = await response.blob();
-
-		return new File([data], 'userPhoto.jpg', { type: 'image/jpeg' });
-	};
-
-	useEffect(() => {
-		if (!user) {
-			history.push('/');
-			return;
-		}
-
-		axios
-			.get('https://api.chatengine.io/users/me', {
-				headers: {
-					'project-id': '7173afc9-7f26-42c4-86cf-90c0e1ae2a97',
-					'user-name': user.email,
-					'user-secret': user.uid,
-				},
-			})
-			.then(() => {
-				setLoading(false);
-			})
-			.catch(() => {
-				let formdata = new FormData();
-				formdata.append('email', user.email);
-				formdata.append('username', user.displayName);
-				formdata.append('secret', user.uid);
-
-				getFile(user.photoURL).then((avatar) => {
-					formdata.append('avatar', avatar, avatar.name);
-
-					axios
-						.post('https://api/chatengine.io/users', formdata, {
-							headers: {
-								'private-key': '8f5b3226-dead-4085-a282-68484b9ef235',
-							},
-						})
-						.then(() => setLoading(false))
-						.catch((error) => console.log(error));
-				});
-			});
-	}, []);
-
-	const handleLogout = async () => {
+	async function handleLogout() {
 		await auth.signOut();
 		history.push('/');
-	};
+	}
 
-	if (!user || loading) return 'Loading';
+	async function getFile(url) {
+		let response = await fetch(url);
+		let data = await response.blob();
+		return new File([data], 'test.jpg', { type: 'image/jpeg' });
+	}
+
+	useEffect(() => {
+		if (!didMountRef.current) {
+			didMountRef.current = true;
+
+			if (!user || user === null) {
+				history.push('/');
+				return;
+			}
+
+			axios
+				.get('https://api.chatengine.io/users/me/', {
+					headers: {
+						'project-id': process.env.REACT_APP_CHAT_ENGINE_ID,
+						'user-name': user.email,
+						'user-secret': user.uid,
+					},
+				})
+
+				.then(() => setLoading(false))
+
+				.catch((e) => {
+					let formdata = new FormData();
+					formdata.append('email', user.email);
+					formdata.append('username', user.email);
+					formdata.append('secret', user.uid);
+
+					getFile(user.photoURL).then((avatar) => {
+						formdata.append('avatar', avatar, avatar.name);
+
+						axios
+							.post('https://api.chatengine.io/users/', formdata, {
+								headers: { 'private-key': process.env.REACT_APP_CHAT_ENGINE_KEY },
+							})
+							.then(() => setLoading(false))
+							.catch((e) => console.log('e', e.response));
+					});
+				});
+			// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		}
+	}, [user, history]);
+
+	if (!user || loading) return <div />;
 
 	return (
 		<div className="chats-page">
 			<div className="nav-bar">
-				<div className="logo-tab">Discube</div>
-				<div className="logout-tab" onClick={handleLogout}>
+				<div className="logo-tab">Unichat</div>
+
+				<div onClick={handleLogout} className="logout-tab">
 					Logout
 				</div>
 			</div>
+
 			<ChatEngine
 				height="calc(100vh - 66px)"
-				projectID="7173afc9-7f26-42c4-86cf-90c0e1ae2a97"
+				projectID={process.env.REACT_APP_CHAT_ENGINE_ID}
 				userName={user.email}
 				userSecret={user.uid}
 			/>
 		</div>
 	);
 };
-
 export default Chats;
