bff73d7bb4feeb162d0dbb886e5ba6087bddfc65
Cody Stine <cydoentis@gmail.com>
2021-10-11 20:26:00 -0400

    Removed facebook login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,20 +11,10 @@ const Login = () => {
 		<div id="login-page">
 			<div id="login-card">
 				<h2>Welcome to Discube!</h2>
-
 				<div
 					className="login-button google"
 					onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}>
 					<GoogleOutlined /> Sign In with Google
-				</div>
-
-				<br />
-				<br />
-
-				<div
-					className="login-button facebook"
-					onClick={() => auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())}>
-					<FacebookOutlined /> Sign In with Facebook
 				</div>
 			</div>
 		</div>
